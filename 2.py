422d336ed3016ddd02276bd920fe3a88c04ada90
Деружинский Дмитрий <91655905+Terebellum@users.noreply.github.com>
2021-11-29 21:36:44 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,9 +3,23 @@
 if __name__ == "__main__":
     n = int(input())
     
-    if 5 < n < 14:
-        print(n)
+    if n == 6:
+        print("шестёрка")
+    elif n == 7:
+        print("семёрка")
+    elif n == 8:
+        print("восьмёрка")
+    elif n == 9:
+        print("девятк")
+    elif n == 10:
+        print("десятка")
+    elif n == 11:
+        print("валет")
+    elif n == 12:
+        print("дама")
+    elif n == 13:
+        print("король")
     elif n == 14:
-        print(17)
+        print("туз")
     else:
         print("нет")
